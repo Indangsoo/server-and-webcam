4eaaea2ad1be12fbc95f6b5a0c06b27f6e78c8a6
min-jp <mincjiip@gmail.com>
2024-11-25 02:19:10 +0900

    server, db 구현완료

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import time
 
 from db_response_model import ResponseModel
 
@@ -16,13 +17,27 @@ class Database:
             with sqlite3.connect(self.db_file) as conn:  # with문으로 conn 사용후 닫음
                 cursor = conn.cursor()
                 cursor.execute('''
-                CREATE TABLE IF NOT EXISTS car (
+                CREATE TABLE IF NOT EXISTS event (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    danger INTEGER
+                    type TEXT NOT NULL,
+                    place TEXT NOT NULL,
+                    key NUMERIC NOT NULL
                 )
-                ''') # car 테이블 생성
-                # TODO
-                #  추가 테이블
+                ''') # event 테이블 생성
+                cursor.execute('''
+                CREATE TABLE IF NOT EXISTS towel (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    num INTEGER NOT NULL,
+                    time NUMERIC NOT NULL
+                )
+                ''') # towel 테이블 생성
+                cursor.execute('''
+                CREATE TABLE IF NOT EXISTS stuff_call (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    key NUMERIC NOT NULL
+                )
+                ''') # stuff_call 테이블 생성
                 conn.commit()  # 저장
                 return ResponseModel(True, None).dict()  # 딕셔너리 형태로 응답
         except sqlite3.OperationalError as e:  # 오류 발생한 경우
@@ -46,28 +61,58 @@ class Database:
         except sqlite3.OperationalError as e:  # 오류 발생한 경우
             return ResponseModel(False, str(e)).dict()
 
-    def get_data(self, table_name, column, order_by):
+    def get_data(self, table_name, find_columns, order_by, search_q=None):
         '''필요 데이터 get'''
         try:  # 예외처리
             with sqlite3.connect(self.db_file) as conn:  # with문으로 conn 사용후 닫음
+                conn.row_factory = sqlite3.Row
+
                 cursor = conn.cursor()
 
-                query = f"SELECT {column} FROM {table_name} ORDER BY {order_by} DESC LIMIT 1"  # 가장 최근 데이터 쿼리
+                columns_str = ", ".join(find_columns)  # 컬럼 이름
+
+                if search_q:
+                    query = f"SELECT {columns_str} FROM {table_name} WHERE {search_q} ORDER BY {order_by} DESC LIMIT 1"  # 가장 최근 데이터 쿼리
+                else:
+                    query = f"SELECT {columns_str} FROM {table_name} ORDER BY {order_by} DESC LIMIT 1"  # 가장 최근 데이터 쿼리
 
                 cursor.execute(query)  # 실행
 
-                data = tuple(cursor.fetchone())  # 최근 데이터 한개
+                row = cursor.fetchone()  # 최근 데이터 한개
 
-                return ResponseModel(True, str(data[0])).dict()  # 딕셔너리 형태로 응답
+                if row:
+                    data = dict(row)
+
+                return ResponseModel(True, str(data)).dict()  # 딕셔너리 형태로 응답
         except sqlite3.OperationalError as e:  # 오류 발생한 경우
             return ResponseModel(False, str(e)).dict()
 
     def insert_car_danger(self, timestamp):
-        '''/car/danger DB 삽입'''
-        return self.insert_data("car", ["danger"], [timestamp])
+        return self.insert_data("event", ["type", "place", "key"], ["danger", "car", timestamp])
 
     def get_car_danger(self):
-        '''/car/danger 값 get'''
-        return self.get_data("car", "danger", "danger")
+        return self.get_data("event", ["key"], "key", "type = 'danger' AND place = 'car'")
 
-    # TODO  # 인서트 함수 추가
+    def insert_car_open(self, timestamp):
+        return self.insert_data("event", ["type", "place", "key"], ["open", "car", timestamp])
+
+    def get_car_open(self):
+        return self.get_data("event", ["key"], "key", "type = 'open' AND place = 'car'")
+
+    def insert_indoor_danger(self, timestamp):
+        return self.insert_data("event", ["type", "place", "key"], ["danger", "indoor", timestamp])
+
+    def get_indoor_danger(self):
+        return self.get_data("event", ["key"], "key", "type = 'danger' AND place = 'indoor'")
+
+    def insert_toilet_towel(self, num):
+        return self.insert_data("towel", ["num", "time"], [num, time.time()])
+
+    def get_toilet_towel(self):
+        return self.get_data("towel", ["num"], "time")
+
+    def insert_stuff_call(self, stuff_name, timestamp):
+        return self.insert_data("stuff_call", ["name", "key"], [stuff_name, timestamp])
+
+    def get_stuff_call(self):
+        return self.get_data("stuff_call", ["name", "key"], "key")
--- server.py
+++ server.py
@@ -16,10 +16,15 @@ db = Database()
 def car_danger():
     timestamp = request.args.get("key")  # 쿼리 데이터
 
+    if "." in timestamp:  # 정수 또는 실수로 변환
+        timestamp = float(timestamp)
+    else:
+        timestamp = int(timestamp)
+
     response = db.insert_car_danger(timestamp)  # DB 삽입
 
     if response.get("success"):  # DB에 정상적으로 삽입된 경우
-        socketio.emit("/car/danger", timestamp)  # 웹소켓으로 정보 보냄
+        socketio.emit("/car/danger", str({"key": timestamp}))  # 웹소켓으로 정보 보냄
 
         return Response(status=200)  # ok
     else:
@@ -34,10 +39,109 @@ def car_danger_data():
     else:
         return Response(status=400)  # Bad
 
+@app.route('/car/open', methods=["GET"])
+def car_open():
+    timestamp = request.args.get("key")  # 쿼리 데이터
 
+    if "." in timestamp:  # 정수 또는 실수로 변환
+        timestamp = float(timestamp)
+    else:
+        timestamp = int(timestamp)
 
-#TODO
-# route 추가
+    response = db.insert_car_open(timestamp)  # DB 삽입
+
+    if response.get("success"):  # DB에 정상적으로 삽입된 경우
+        socketio.emit("/car/open", str({"key": timestamp}))  # 웹소켓으로 정보 보냄
+
+        return Response(status=200)  # ok
+    else:
+        return Response(response.get("data"), status=400)  # Bad
+
+@app.route('/car/open/data', methods=["GET"])
+def car_open_data():
+    response = db.get_car_open()  # data get
+
+    if response.get("success"):  # DB에 정상적으로 삽입된 경우
+        return Response(response.get("data") ,status=200)  # ok
+    else:
+        return Response(status=400)  # Bad
+
+@app.route('/indoor/danger', methods=["GET"])
+def indoor_danger():
+    timestamp = request.args.get("key")  # 쿼리 데이터
+
+    if "." in timestamp:  # 정수 또는 실수로 변환
+        timestamp = float(timestamp)
+    else:
+        timestamp = int(timestamp)
+
+    response = db.insert_indoor_danger(timestamp)  # DB 삽입
+
+    if response.get("success"):  # DB에 정상적으로 삽입된 경우
+        socketio.emit("/indoor/danger", str({"key": timestamp}))  # 웹소켓으로 정보 보냄
+
+        return Response(status=200)  # ok
+    else:
+        return Response(response.get("data"), status=400)  # Bad
+
+@app.route('/indoor/danger/data', methods=["GET"])
+def indoor_danger_data():
+    response = db.get_indoor_danger()  # data get
+
+    if response.get("success"):  # DB에 정상적으로 삽입된 경우
+        return Response(response.get("data"), status=200)  # ok
+    else:
+        return Response(status=400)  # Bad
+
+@app.route('/toilet/towel', methods=["GET"])
+def toilet_towel():
+    num = int(request.args.get("num"))  # 쿼리 데이터
+
+    response = db.insert_toilet_towel(num)  # DB 삽입
+
+    if response.get("success"):  # DB에 정상적으로 삽입된 경우
+        socketio.emit("/toilet/towel", str({"num": num}))  # 웹소켓으로 정보 보냄
+
+        return Response(status=200)  # ok
+    else:
+        return Response(response.get("data"), status=400)  # Bad
+
+@app.route('/toilet/towel/data', methods=["GET"])
+def toilet_towel_data():
+    response = db.get_toilet_towel()  # data get
+
+    if response.get("success"):  # DB에 정상적으로 삽입된 경우
+        return Response(response.get("data"), status=200)  # ok
+    else:
+        return Response(status=400)  # Bad
+
+@app.route('/stuff/call', methods=["GET"])
+def stuff_call():
+    name = request.args.get("name")
+    timestamp = request.args.get("key")  # 쿼리 데이터
+
+    if "." in timestamp:  # 정수 또는 실수로 변환
+        timestamp = float(timestamp)
+    else:
+        timestamp = int(timestamp)
+
+    response = db.insert_stuff_call(name, timestamp)  # DB 삽입
+
+    if response.get("success"):  # DB에 정상적으로 삽입된 경우
+        socketio.emit("/stuff/call", str({"name": name, "key": timestamp}))  # 웹소켓으로 정보 보냄
+
+        return Response(status=200)  # ok
+    else:
+        return Response(response.get("data"), status=400)  # Bad
+
+@app.route('/stuff/call/data', methods=["GET"])
+def stuff_call_data():
+    response = db.get_stuff_call()  # data get
+
+    if response.get("success"):  # DB에 정상적으로 삽입된 경우
+        return Response(response.get("data"), status=200)  # ok
+    else:
+        return Response(status=400)  # Bad
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
